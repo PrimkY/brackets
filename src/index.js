c1ffa755626dc715def609d70fc1ebb0365f9785
PrimkY <fakeprimky@gmail.com>
2021-03-13 10:54:55 +0300

    init: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strBC = bracketsConfig.flat(1).join('');
+  let stack = [];
+  let index;
+  for(let i = 0; i < str.length; i++){
+      index = strBC.indexOf(str[i]);
+      if(index !== -1){
+          if(index % 2 === 0){
+              if(strBC[index+1] === str[i] && stack[stack.length-1] === str[i]) stack.pop();
+              else stack.push(str[i]);
+          }
+          else if(stack.length !== 0 && stack[stack.length - 1] === strBC[index-1]) stack.pop();
+          else return false;
+      }
+      else return false;
+  }
+  if(stack.length === 0) return true;
+  else return false;
 }
